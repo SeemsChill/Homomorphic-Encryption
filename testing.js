be4c3f22b3a7067ef0df38cf6d09e6fa7312067d
TheForwarding <ilovesaigonbefore1975@gmail.com>
2021-04-18 11:12:20 +0700

    Advanced Changing

--- testing.js
+++ testing.js
@@ -1,6 +1,4 @@
 var Eratosthenes = require("./sieve_of_eratosthenes");
 
-const something = new Eratosthenes(2, 50);
-
-something.generating_list(2, 50);
+const something = new Eratosthenes(1000, 10000);
 something.out_put();
